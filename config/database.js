e0eb43b9c456528ef21a98ca5fbb8570bc06f133
Alok <alkmohanty@gmail.com>
2017-08-27 13:47:47 -0700

    localset up done

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ module.exports = {
 
 	// the database url to connect
 	/*url : 'mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'*/
-	url : 'mongodb://localhost/webrefs'
+	url : 'mongodb://localhost/wefrenz'
 }
