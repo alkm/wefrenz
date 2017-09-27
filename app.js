caa4f973b768e0c9db01228d15b407f6f3b28531
Alok <alkmohanty@gmail.com>
2017-09-27 13:37:08 -0700

    port changed

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ mongoose.connect(database.url, { useMongoClient: true }, function(err){
 
 	var https = require('https');
 	var fs = require('fs');
-	var port = 8080;
+	var port = 3000;
 
 	/*app.get('/', (req, res) => {
 	  res.send('WORKING!')
